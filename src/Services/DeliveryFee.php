67fc7ddf2265ddf73ebfcad4065277a0316e328a
Emily Shepherd <emily@emilyshepherd.me>
2020-04-05 22:19:57 +0100

    Add DeliveryFee implementation of PriceModifier
    DeliveryFee is a PriceModifier which adds the appropriate Delivery Fee
    line item, based on the overall price

---
+++ src/Services/DeliveryFee.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AcmeWidget\Services;
+
+use AcmeWidget\Interfaces\Basket;
+use AcmeWidget\Interfaces\Invoice;
+use AcmeWidget\Interfaces\PriceModifier;
+
+/**
+ * Adds the delivery fee to the order
+ */
+class DeliveryFee implements PriceModifier
+{
+    /**
+     * An array of threshold values and the delivery fee of the order if
+     * the total is less than this threshold
+     *
+     * @var array
+     */
+    private $thresholds = [ ];
+
+    /**
+     * Sets up the DeliveryFee modifier with a list of thresholds
+     *
+     * @param array $thresholds
+     */
+    public function __construct(array $thresholds)
+    {
+        $this->thresholds = $thresholds;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function processRules(
+        Basket $basket,
+        Invoice $invoice
+    ): Invoice
+    {
+        $totalPrice = $invoice->getTotal();
+        $delivery = 0;
+
+        foreach ($this->thresholds as $threshold => $cost)
+        {
+            if ($totalPrice < $threshold)
+            {
+                $delivery = $cost;
+                break;
+            }
+        }
+
+        return $invoice->addLineItem('Delivery Fee', $delivery);
+    }
+}
---
+++ test/Services/DeliveryFeeTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace AcmeWidget\Test\Entities;
+
+use AcmeWidget\Interfaces\Basket;
+use AcmeWidget\Services\DeliveryFee;
+use AcmeWidget\Entities\SimpleInvoice;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Simple set of unit tests to ensure the DeliveryFee modifier works as
+ * expected
+ */
+class DeliveryFeeTest extends TestCase
+{
+    /**
+     * An instance of the DeliveryFee modifier to test
+     *
+     * @var DeliveryFee
+     */
+    private $deliveryFee;
+
+    /**
+     * A mocked instance of a Basket - it doesn't do anything, but the
+     * requirement is there for the PriceModifier::processRules() method
+     */
+    private $basket;
+
+    /**
+     * Sets up each test by creating a DeliveryFee modifier with the
+     * sample thresholds given in the spec
+     */
+    public function setUp(): void
+    {
+        $this->deliveryFee = new DeliveryFee([
+            5000 => 495,
+            9000 => 295,
+        ]);
+        $this->basket = $this->getMockBuilder(Basket::class)
+            ->getMock();
+    }
+
+    /**
+     * Tests the order with various costs to ensure the correct delivery
+     * fee is added
+     *
+     * @dataProvider deliveryTestProvider
+     * @param int $cost The invoice cost pre delivery to test with
+     * @param int $fee The expected delivery fee
+     */
+    public function testFee(int $cost, int $fee): void
+    {
+        $items = $this->deliveryFee->processRules(
+            $this->basket,
+            (new SimpleInvoice())->addLineItem('Test', $cost)
+        )->getLineItems();
+
+        $this->assertCount(2, $items);
+        $this->assertSame('Delivery Fee', $items[1]['description']);
+        $this->assertSame($fee, $items[1]['cost']);
+    }
+
+    /**
+     * Provides test data for the fee test (testFee above)
+     *
+     * @return array
+     */
+    public function deliveryTestProvider(): array
+    {
+        return [
+            [1000, 495], // Orders under $50 => delivery is $4.25
+            [8100, 295], // Orders under $90 => delivery is $2.95
+            [10600, 0], // Orders over $90 => delivery is free
+            [9000, 0], // Orders of exactly $90 => delivery is free
+        ];
+    }
+}
